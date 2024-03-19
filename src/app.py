050290a32dde6731b8e9278756e029c372c18834
sanikk <sanikk@users.noreply.github.com>
2024-03-19 23:06:28 +0200

    layout, map_page work

--- src/app.py
+++ src/app.py
@@ -17,4 +17,9 @@ def index():
 
 @app.route("/map")
 def map_view():
-    return render_template("map.html")
+    return render_template("map_page.html", mappi="test_map_markers.html")
+
+
+@app.route("/map2")
+def map2_view():
+    return render_template("test_map_markers.html")
