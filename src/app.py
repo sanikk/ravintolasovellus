c6d5e665425c1a7d3f79a2eb52e3ada7098b2a0e
Samuli Nikkilä <sanikk@users.noreply.github.com>
2025-01-20 13:55:49 +0200

    filter template to display user names
    added output_account_name
    on pages:
    ratings_single

--- src/app.py
+++ src/app.py
@@ -1,5 +1,5 @@
 from config import DATABASE_NAME, FLASK_SECRET_KEY
-from filters import output_date, output_datetime, output_rating_stars
+from filters import output_date, output_datetime, output_rating_stars, output_account_name
 from flask import Flask
 from markupsafe import Markup
 from flask_sqlalchemy import SQLAlchemy
@@ -16,6 +16,7 @@ app.config["SQLALCHEMY_DATABASE_URI"] = "postgresql+psycopg:///" + DATABASE_NAME
 app.add_template_filter(output_date, "output_date")
 app.add_template_filter(output_datetime, "output_datetime")
 app.add_template_filter(output_rating_stars, "output_rating_stars")
+app.add_template_filter(output_account_name, "output_account_name")
 
 db = SQLAlchemy(app)
 
--- src/filters.py
+++ src/filters.py
@@ -13,4 +13,12 @@ def output_rating_stars(rating: int):
     star_icon = '<img src="/static/icons8-star-50.png" alt="Star" />'
     return Markup(star_icon * rating)
 
+
+def output_account_name(datadict):
+    if "firstname" in datadict._mapping or "lastname" in datadict._mapping:
+        return datadict._mapping.get("firstname", "") or datadict.get("lastname", "")
+    if "account_firstname" in datadict._mapping or "account_lastname" in datadict._mapping:
+        return datadict._mapping.get("account_firstname", "") or datadict._mapping.get("account_lastname", "")
+    return ""
+
 # <a target="_blank" href="https://icons8.com/icon/104/star">Star</a> icon by <a target="_blank" href="https://icons8.com">Icons8</a>
