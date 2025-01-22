34455ef986419b8675b8d41ef6ddd2117bb006b5
Samuli Nikkilä <sanikk@users.noreply.github.com>
2025-01-22 06:07:43 +0200

    added weekday names as context to templates

--- src/app.py
+++ src/app.py
@@ -1,8 +1,14 @@
 from config import DATABASE_NAME, FLASK_SECRET_KEY
-from filters import output_date, output_datetime, output_rating_stars, output_account_name
+from filters import (
+    output_date,
+    output_datetime,
+    output_rating_stars,
+    output_account_name,
+    output_full_name,
+)
 from flask import Flask
-from markupsafe import Markup
 from flask_sqlalchemy import SQLAlchemy
+from calendar import calendar, day_name
 
 
 app = Flask(__name__)
@@ -13,10 +19,19 @@ if not DATABASE_NAME or not FLASK_SECRET_KEY:
 app.secret_key = FLASK_SECRET_KEY
 app.config["SQLALCHEMY_DATABASE_URI"] = "postgresql+psycopg:///" + DATABASE_NAME
 
+# add template filters to be used on jinja2 templates
 app.add_template_filter(output_date, "output_date")
 app.add_template_filter(output_datetime, "output_datetime")
 app.add_template_filter(output_rating_stars, "output_rating_stars")
 app.add_template_filter(output_account_name, "output_account_name")
+app.add_template_filter(output_full_name, "output_full_name")
+
+
+# add constants to be used on jinja2 templates
+@app.context_processor
+def inject_weekdays():
+    return {"weekdays": day_name}
+
 
 db = SQLAlchemy(app)
 
--- src/filters.py
+++ src/filters.py
@@ -1,4 +1,5 @@
 from markupsafe import Markup
+
 # template filters used on the jinja pages. these are added in app.
 
 
@@ -9,16 +10,24 @@ def output_date(the_date):
 def output_datetime(the_datetime):
     return the_datetime.strftime("%A %d. %m. %Y %I:%M")
 
-def output_rating_stars(rating: int):
-    star_icon = '<img src="/static/icons8-star-50.png" alt="Star" />'
+
+def output_rating_stars(rating: int, size: int = 30):
+    star_icon = f'<img src="/static/icons8-star-50.png" alt="Star" style="width:{size}px;height:{size}px;" />'
     return Markup(star_icon * rating)
 
 
-def output_account_name(datadict):
-    if "firstname" in datadict._mapping or "lastname" in datadict._mapping:
-        return datadict._mapping.get("firstname", "") or datadict.get("lastname", "")
-    if "account_firstname" in datadict._mapping or "account_lastname" in datadict._mapping:
-        return datadict._mapping.get("account_firstname", "") or datadict._mapping.get("account_lastname", "")
+def output_account_name(row):
+    if "firstname" in row._mapping or "lastname" in row._mapping:
+        return row._mapping.get("firstname", "") or row._mapping.get("lastname", "")
+    if "account_firstname" in row._mapping or "account_lastname" in row._mapping:
+        return row._mapping.get("account_firstname", "") or row._mapping.get(
+            "account_lastname", ""
+        )
     return ""
+
+
+def output_full_name(row):
+    return f"{row._mapping.get("firstname", "")} {row._mapping.get("lastname", "")}".strip()
+
 
 # <a target="_blank" href="https://icons8.com/icon/104/star">Star</a> icon by <a target="_blank" href="https://icons8.com">Icons8</a>
